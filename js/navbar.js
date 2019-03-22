4b2d525592bd9daa454cd7dafa4fd19fc8a856d0
hansung choi <truper12@prorataart.com>
2019-03-22 18:14:47 +0900

    init

---
+++ js/navbar.js
@@ -0,0 +1,23 @@
+$(document).ready(function() {
+    // nav bar - login/out
+    var tk = sessionStorage.getItem("azblr-tk")
+    var _headers = {}
+    _headers['Authorization'] = tk
+    if (tk == null) {
+        $('#login-button').removeClass('d-none')
+    } else {
+        $('#logout-button').removeClass('d-none')
+        $('#logout-button').click(function() {
+            $.ajax({
+                url: API_HOST+'/api/auth/logout',
+                type: 'post',
+                headers: _headers
+            })
+            sessionStorage.clear()
+            location.reload()
+        })
+    }
+    $(document).ready(function(){
+        $("#man-modal").load("/man-modal.html")       
+    });
+})
